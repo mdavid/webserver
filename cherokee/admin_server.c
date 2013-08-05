66526b83935c8bf1dff06a1eb780fc70dd109a2a
Birk Borkason <daniel.niccoli@gmail.com>
2013-08-05 11:21:39 +0200

    Update admin_server.c

--- cherokee/admin_server.c
+++ cherokee/admin_server.c
@@ -396,7 +396,7 @@ cherokee_admin_server_reply_get_sources (cherokee_handler_t *hdl,
 	cherokee_server_t *srv = HANDLER_SRV(hdl);
 
 	cherokee_dwriter_list_open (dwriter);
-	cherokee_avl_while (&srv->sources, sources_while, dwriter, NULL, NULL);
+	cherokee_avl_while (*(cherokee_avl_generic_t **)&srv->sources, sources_while, dwriter, NULL, NULL);
 	cherokee_dwriter_list_close (dwriter);
 
 	return ret_ok;
